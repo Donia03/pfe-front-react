daa2ffb7d3c8c327e2ca41d5533c483bebd23939
mouezchouchene <mouez.chouchene@gmail.com>
2023-09-10 22:56:51 +0200

    modifier mot de pass

--- src/pages/profil/Profil.jsx
+++ src/pages/profil/Profil.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import {
   CalendarToday,
   LocationSearching,
@@ -8,135 +8,176 @@ import {
   PhoneAndroid,
   Publish,
 } from "@material-ui/icons";
-import { Link, useParams} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./Profil.css";
 
 export default function Profil() {
-
   const params = useParams();
-  console.log(params.userId)
+  console.log(params.userId);
 
-  const [email, setEmail] = useState('')
-  const [prenom, setPrenom] = useState('')
-  const [nom, setNom] = useState('')
-  const [password, setPassword] = useState('')
-  const [cin, setCin] = useState('')
-  const[image, setImage] = useState('')
-  const[imageUrl, setImageUrl] = useState('')
-  const[telephone, setTelephone] = useState('')
-
-   useEffect(() => {
-      getUserById()
-    },[]);
-
-    useEffect(() => {
-        if (image) {
-          getUserImage();
-        }
-      }, [image]);
+  const [email, setEmail] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [nom, setNom] = useState("");
+  const [password, setPassword] = useState("");
+  const [cin, setCin] = useState("");
+  const [image, setImage] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
 
+  useEffect(() => {
+    getUserById();
+  }, []);
 
-const token = localStorage.getItem('token');
+  useEffect(() => {
+    if (image) {
+      getUserImage();
+    }
+  }, [image]);
 
- const getUserById = async () => {
-    const result = await axios.get("http://localhost:8082/api/user/"+params.userId,{
+  const token = localStorage.getItem("token");
+
+  const getUserById = async () => {
+    const result = await axios.get(
+      "http://localhost:8082/api/user/" + params.userId,
+      {
         headers: {
-        "Authorization": `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
         },
-    })
-    console.log("hetha l obket jibneh mel api",result)
-    const data = result.data
-    setEmail(data.email)
-    setNom(data.nom)
-    setPrenom(data.prenom)
-    setCin(data.cin)
-    setImage(data.imagePath)
-    setTelephone(data.telephone)
-  }
+      }
+    );
+    console.log("hetha l obket jibneh mel api", result);
+    const data = result.data;
+    setEmail(data.email);
+    setNom(data.nom);
+    setPrenom(data.prenom);
+    setCin(data.cin);
+    setImage(data.imagePath);
+    setTelephone(data.telephone);
+  };
 
   const getUserImage = async () => {
-      const result = await axios.get("http://localhost:8082/api/images/" + image, {
-        headers: {
-          "Authorization": `Bearer ${token}`,
-        },
-        responseType: "blob", // Tell Axios to handle the response as a blob (binary data)
-      });
+    const result = await axios.get("http://localhost:8082/api/images/" + image, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      responseType: "blob", // Tell Axios to handle the response as a blob (binary data)
+    });
 
-      const blob = result.data;
+    const blob = result.data;
 
-      // Convert the blob to a base64-encoded URL
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        const base64data = reader.result;
-        setImageUrl(base64data);
-      };
-      reader.readAsDataURL(blob);
+    // Convert the blob to a base64-encoded URL
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const base64data = reader.result;
+      setImageUrl(base64data);
     };
+    reader.readAsDataURL(blob);
+  };
 
   const emailChangeHandler = (event) => {
-    setEmail(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
   const telephoneChangeHandler = (event) => {
-    setTelephone(event.target.value)
-  }
+    setTelephone(event.target.value);
+  };
   const nomChangeHandler = (event) => {
-  console.log("hetha l objet event mta3 changehandler ",event)
-    setNom(event.target.value)
-  }
+    console.log("hetha l objet event mta3 changehandler ", event);
+    setNom(event.target.value);
+  };
   const prenomChangeHandler = (event) => {
-    setPrenom(event.target.value)
-  }
+    setPrenom(event.target.value);
+  };
   const passwordChangeHandler = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
   const cinChangeHandler = (event) => {
-      setCin(event.target.value)
-    }
+    setCin(event.target.value);
+  };
+  const oldPasswordChangeHandler = (event) => {
+    setOldPassword(event.target.value);
+  };
 
-   const handleImageChange = (file) => {
-     if (file) {
-       setImage(file);
-       const reader = new FileReader();
-       reader.onload = () => {
-         setImageUrl(reader.result);
-       };
-       reader.readAsDataURL(file);
-     }
-   };
+  const newPasswordChangeHandler = (event) => {
+    setNewPassword(event.target.value);
+  };
+
+
+  const handleImageChange = (file) => {
+    if (file) {
+      setImage(file);
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImageUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const formData = new FormData();
-      formData.append("nom", nom);
-      formData.append("prenom", prenom);
-      formData.append("email", email);
-      formData.append("cin", cin);
-      formData.append("telephone", telephone);
-      if (image) {
-        formData.append("image", image);
-      }
+    formData.append("nom", nom);
+    formData.append("prenom", prenom);
+    formData.append("email", email);
+    formData.append("cin", cin);
+    formData.append("telephone", telephone);
+    if (image) {
+      formData.append("image", image);
+    }
 
-      try {
-        const response = await axios.put(
-          `http://localhost:8082/api/user/${params.userId}`,
-          formData,
-          {
-            headers: {
-              "Authorization": `Bearer ${token}`,
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
+    try {
+      const response = await axios.put(
+        `http://localhost:8082/api/user/${params.userId}`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
 
-        // Handle the response, e.g., show success message, redirect, etc.
-        console.log("User updated:", response.data);
-      } catch (error) {
-        // Handle error, e.g., show error message
-        console.error("Error updating user:", error);
-      }
-  }
+      // Handle the response, e.g., show success message, redirect, etc.
+      console.log("User updated:", response.data);
+    } catch (error) {
+      // Handle error, e.g., show error message
+      console.error("Error updating user:", error);
+    }
+  };
+  const handleChangePassword = async (e) => {
+    e.preventDefault();
+
+    // Create an object to send to the API
+    const passwordData = {
+      oldPass: oldPassword,
+      newPass: newPassword,
+    };
+
+    try {
+      const response = await axios.post(
+        `http://localhost:8082/api/changerPassword/${params.userId}`,
+        passwordData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      // Handle the response, e.g., show success message, redirect, etc.
+      console.log("Password changed:", response.data);
+      setOldPassword('');
+      setNewPassword('');
+    } catch (error) {
+      // Handle error, e.g., show error message
+      console.error("Error changing password:", error);
+    }
+  };
+
 
   return (
     <div className="user">
@@ -146,13 +187,9 @@ const token = localStorage.getItem('token');
       <div className="userContainer">
         <div className="userShow">
           <div className="userShowTop">
-            <img
-              src={imageUrl}
-              alt=""
-              className="userShowImg"
-            />
+            <img src={imageUrl} alt="" className="userShowImg" />
             <div className="userShowTopTitle">
-              <span className="userShowUsername">{nom +' '+ prenom}</span>
+              <span className="userShowUsername">{nom + " " + prenom}</span>
               <span className="userShowUserTitle">Welcome</span>
             </div>
           </div>
@@ -166,10 +203,10 @@ const token = localStorage.getItem('token');
               <PermIdentity className="userShowIcon" />
               <span className="userShowInfoTitle">{nom}</span>
             </div>
-             <div className="userShowInfo">
-                          <PermIdentity className="userShowIcon" />
-                          <span className="userShowInfoTitle">{cin}</span>
-                        </div>
+            <div className="userShowInfo">
+              <PermIdentity className="userShowIcon" />
+              <span className="userShowInfoTitle">{cin}</span>
+            </div>
 
             <span className="userShowTitle">Contact </span>
             <div className="userShowInfo">
@@ -180,7 +217,6 @@ const token = localStorage.getItem('token');
               <MailOutline className="userShowIcon" />
               <span className="userShowInfoTitle">{email}</span>
             </div>
-
           </div>
         </div>
         <div className="userUpdate">
@@ -220,28 +256,28 @@ const token = localStorage.getItem('token');
                   onChange={emailChangeHandler}
                 />
               </div>
-               <div className="userUpdateItem">
-                              <label>Cin</label>
-                              <input
-                                type="text"
-                                placeholder=""
-                                className="userUpdateInput"
-                                name={"cin"}
-                                value={cin}
-                                onChange={cinChangeHandler}
-                              />
-                            </div>
-            <div className="userUpdateItem">
-                                                       <label>Telephone</label>
-                                                       <input
-                                                         type="text"
-                                                         placeholder=""
-                                                         className="userUpdateInput"
-                                                         name={"telephone"}
-                                                         value={telephone}
-                                                         onChange={telephoneChangeHandler}
-                                                       />
-                                                     </div>
+              <div className="userUpdateItem">
+                <label>Cin</label>
+                <input
+                  type="text"
+                  placeholder=""
+                  className="userUpdateInput"
+                  name={"cin"}
+                  value={cin}
+                  onChange={cinChangeHandler}
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Telephone</label>
+                <input
+                  type="text"
+                  placeholder=""
+                  className="userUpdateInput"
+                  name={"telephone"}
+                  value={telephone}
+                  onChange={telephoneChangeHandler}
+                />
+              </div>
             </div>
             <div className="userUpdateRight">
               <div className="userUpdateUpload">
@@ -253,13 +289,49 @@ const token = localStorage.getItem('token');
                 <label htmlFor="file">
                   <Publish className="userUpdateIcon" />
                 </label>
-                <input type="file" id="file" style={{ display: "none" }} onChange={(e) => handleImageChange(e.target.files[0])} />
+                <input
+                  type="file"
+                  id="file"
+                  style={{ display: "none" }}
+                  onChange={(e) => handleImageChange(e.target.files[0])}
+                />
               </div>
-              <button type="submit" className="userUpdateButton">Enregistrer</button>
+              <button type="submit" className="userUpdateButton">
+                Enregistrer
+              </button>
             </div>
           </form>
         </div>
       </div>
+      <div className="passwordChange">
+        <span className="userUpdateTitle">Changer mot de passe</span>
+        <form onSubmit={handleChangePassword} className="userUpdateForm">
+          <div className="userUpdateItem">
+            <label>Ancien mot de passe</label>
+            <input
+              type="password"
+              placeholder=""
+              className="userUpdateInput"
+              value={oldPassword}
+              onChange={oldPasswordChangeHandler}
+            />
+          </div>
+          <div className="userUpdateItem">
+            <label>Nouveau mot de passe</label>
+            <input
+              type="password"
+              placeholder=""
+              className="userUpdateInput"
+              value={newPassword}
+              onChange={newPasswordChangeHandler}
+            />
+          </div>
+          <button type="submit" className="userUpdateButton">
+            Enregistrer
+          </button>
+        </form>
+      </div>
+
     </div>
   );
 }
