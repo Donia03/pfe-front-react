85700ab4f8954f1bd744b4f0ca079121bd46f213
mouezchouchene <mouez.chouchene@gmail.com>
2023-09-16 14:17:45 +0200

    add style to note and fix edit

--- src/pages/note/card/Card.jsx
+++ src/pages/note/card/Card.jsx
@@ -1,62 +1,75 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import EditTask from '../editTask/EditTask';
-import './Card.css'
-const Card = ({taskObj, index, deleteTask, updateListArray}) => {
-    const [modal, setModal] = useState(false);
+import './Card.css';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import ScheduleIcon from '@material-ui/icons/Schedule';
 
-    const colors = [
-        {
-            primaryColor : "#5D93E1",
-            secondaryColor : "#ECF3FC"
-        },
-        {
-            primaryColor : "#F9D288",
-            secondaryColor : "#FEFAF1"
-        },
-        {
-            primaryColor : "#5DC250",
-            secondaryColor : "#F2FAF1"
-        },
-        {
-            primaryColor : "#F48687",
-            secondaryColor : "#FDF1F1"
-        },
-        {
-            primaryColor : "#B964F7",
-            secondaryColor : "#F3F0FD"
-        }
-    ]
+const Card = ({ taskObj, index, deleteTask, updateListArray }) => {
+  const [modal, setModal] = useState(false);
 
-    const toggle = () => {
-        setModal(!modal);
+  const colors = [
+    {
+      primaryColor: "#5D93E1",
+      secondaryColor: "#ECF3FC"
+    },
+    {
+      primaryColor: "#F9D288",
+      secondaryColor: "#FEFAF1"
+    },
+    {
+      primaryColor: "#5DC250",
+      secondaryColor: "#F2FAF1"
+    },
+    {
+      primaryColor: "#F48687",
+      secondaryColor: "#FDF1F1"
+    },
+    {
+      primaryColor: "#B964F7",
+      secondaryColor: "#F3F0FD"
     }
+  ];
 
-    const updateTask = (obj) => {
-        updateListArray(obj, index)
-    }
+  const toggle = () => {
+    setModal(!modal);
+  };
 
-    const handleDelete = () => {
-        deleteTask(index)
-    }
+  const updateTask = (obj) => {
+    updateListArray(obj, index);
+  };
 
-    return (
-        <div class = "card-wrapper  ">
-            <div class = "card-top" style={{"background-color": colors[index%5].primaryColor}}>
+  const handleDelete = () => {
+    deleteTask(index);
+  };
 
+  return (
+    <div className="card-wrapper">
+      <div className="card-top" style={{ backgroundColor: colors[index % 5].primaryColor }}></div>
+      <div className="task-holder">
+        <span className="card-header" style={{ backgroundColor: colors[index % 5].primaryColor, borderRadius: "10px" }}>{taskObj.titre}</span>
+        <div className="description-wrapper">
+          <p className="description">{taskObj.description}</p>
+        </div>
+        <div className="icons-time-container">
+
+          <div className="time">
+            <div>
+              <AccessTimeIcon style={{ color: 'yellow' }} /> {taskObj.start}
             </div>
-            <div class = "task-holder">
-                <span class = "card-header" style={{"background-color": colors[index%5].secondaryColor, "border-radius": "10px"}}>{taskObj.titre}</span>
-                <p className = "mt-3">{taskObj.description}</p>
-
-                <div style={{"position": "absolute", "right" : "20px", "bottom" : "20px"}}>
-                    <i class = "far fa-edit mr-3" style={{"color" : colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {() => setModal(true)}></i>
-                    <i class="fas fa-trash-alt" style = {{"color" : colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {handleDelete}></i>
-                </div>
+            <div>
+              <ScheduleIcon style={{ color: 'red' }} /> {taskObj.end}
+            </div>
+          </div>
+          <div className="icons">
+                      <i className="far fa-edit mr-3" style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }} onClick={() => setModal(true)}></i>
+                      <i className="fas fa-trash-alt" style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }} onClick={handleDelete}></i>
+                    </div>
         </div>
-        <EditTask modal={modal} toggle={toggle} updateTask={updateListArray} taskObj={taskObj}/>
-        </div>
-
-    );
+      </div>
+      <EditTask modal={modal} toggle={toggle} updateTask={updateListArray} taskObj={taskObj} />
+      <div className="card-bottom" style={{ backgroundColor: colors[index % 5].primaryColor }}></div>
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/note/editTask/EditTask.jsx
+++ src/pages/note/editTask/EditTask.jsx
@@ -1,80 +1,122 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './EditTask.css';
 
+const EditTask = ({ modal, toggle, updateTask, taskObj }) => {
+  const [titre, setTitre] = useState(taskObj.titre);
+  const [description, setDescription] = useState(taskObj.description);
+  const [startDate, setStartDate] = useState(taskObj.start);
+  const [endDate, setEndDate] = useState(taskObj.end);
 
- const EditTask = ({modal, toggle,updateTask, taskObj}) => {
- const [titre, setTitre] = useState(taskObj.titre);
- const [description, setDescription] = useState(taskObj.description);
+  const handleTitreChange = (e) => {
+    setTitre(e.target.value);
+  };
 
-const handleTitreChange = ( e) => {
-    setTitre(e.target.value)
-}
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
 
-const handleDescriptionChange = ( e) => {
-    setDescription(e.target.value)
-}
+  const handleStartDateChange = (e) => {
+    setStartDate(e.target.value);
+  };
 
-const handleUpdate = async () => {
-  try {
-    const updatedTaskObj = {
-      titre: titre,
-      description: description,
-    };
+  const handleEndDateChange = (e) => {
+    setEndDate(e.target.value);
+  };
 
-    // Send a PUT request to update the task
-    const response = await axios.put(`http://localhost:8082/api/note/${taskObj.id}`, updatedTaskObj); // Replace with your API endpoint and task ID
+  const handleUpdate = async () => {
+    try {
+      const updatedTaskObj = {
+        titre: titre,
+        description: description,
+        start: startDate,
+        end: endDate,
+      };
 
-    // Check if the update was successful (you may need to adjust this based on your API response)
-    if (response.status === 200) {
-      // Call the updateTask function to update the task in the parent component
-      if (typeof updateTask === 'function') {
-        updateTask(updatedTaskObj);
+      // Send a PUT request to update the task
+      const response = await axios.put(
+        `http://localhost:8082/api/note/${taskObj.id}`,
+        updatedTaskObj
+      ); // Replace with your API endpoint and task ID
+
+      // Check if the update was successful (you may need to adjust this based on your API response)
+      if (response.status === 200) {
+        // Call the updateTask function to update the task in the parent component
+        if (typeof updateTask === 'function') {
+          updateTask(updatedTaskObj);
+        }
+
+        toggle(); // Close the modal or perform any other desired action
       }
-
-      toggle(); // Close the modal or perform any other desired action
+    } catch (error) {
+      // Handle errors here (e.g., display an error message)
+      console.error('Error updating task:', error);
     }
-  } catch (error) {
-    // Handle errors here (e.g., display an error message)
-    console.error('Error updating task:', error);
-  }
+  };
+
+  return (
+    <Modal isOpen={modal} toggle={toggle}>
+      <ModalHeader toggle={toggle}>Update Task</ModalHeader>
+      <ModalBody>
+        <form>
+          <div className="form">
+            <label>Name :</label>
+            <input
+              type="text"
+              className="custom-input"
+              placeholder="Task Name"
+              value={titre}
+              onChange={handleTitreChange}
+              name="taskName"
+            />
+          </div>
+
+          <div className="form">
+            <label>Description :</label>
+            <textarea
+              rows="4"
+              className="custom-textarea"
+              placeholder="Task Description"
+              value={description}
+              onChange={handleDescriptionChange}
+              name="description"
+            />
+          </div>
+
+          <div className="form">
+            <label>Start Date :</label>
+            <input
+              type="datetime-local"
+              className="custom-input"
+              value={startDate}
+              onChange={handleStartDateChange}
+              name="startDate"
+            />
+          </div>
+
+          <div className="form">
+            <label>End Date :</label>
+            <input
+              type="datetime-local"
+              className="custom-input"
+              value={endDate}
+              onChange={handleEndDateChange}
+              name="endDate"
+            />
+          </div>
+        </form>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onClick={handleUpdate}>
+          Update
+        </Button>{' '}
+        <Button color="secondary" onClick={toggle}>
+          Cancel
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 };
 
-
-
-
-
-
- return (
-
- <Modal isOpen={modal} toggle={toggle} >
-         <ModalHeader toggle={toggle}>update Task</ModalHeader>
-         <ModalBody>
-           <form>
-           <div className ="form">
-           <label>Name :</label>
-            <input type="text" className="custom-input" placeholder="Champ de texte" value={titre} onChange = {handleTitreChange} name= "taskName"/>
-            
-           </div>
-
-           <div className ="form">
-           <label>Description :</label>
-           <textarea rows="4" className="custom-textarea" placeholder="Zone de texte" value={description} onChange = {handleDescriptionChange} name="description"/>
-           </div>
-           </form>
-         </ModalBody>
-         <ModalFooter>
-           <Button color="primary" onClick={handleUpdate}>
-             Update
-           </Button>{' '}
-           <Button color="secondary" onClick={toggle}>
-             Cancel
-           </Button>
-         </ModalFooter>
-       </Modal>
-
- );
- };
-    export default EditTask;
+export default EditTask;
