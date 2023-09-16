9bc0f7a038b4a4f3313fa6192084fe0c378c32f3
mouezchouchene <mouez.chouchene@gmail.com>
2023-09-16 20:48:08 +0200

    fixing refrech diffusionLists

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {AuthProvider} from "./context/AuthContext";
 import { SelectedDiffusionListProvider } from './context/SelectedDiffusionListContext'; // Import the context
+import { DiffusionsProvider } from './context/DiffusionsContext'; // Import the context
 import './global.css';
 
 ReactDOM.render(
   <React.StrictMode>
-  <AuthProvider>
-  <SelectedDiffusionListProvider>
-    <App />
-  </SelectedDiffusionListProvider>
-  </AuthProvider>
+    <AuthProvider>
+      <SelectedDiffusionListProvider>
+        <DiffusionsProvider>
+          <App />
+        </DiffusionsProvider>
+      </SelectedDiffusionListProvider>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
